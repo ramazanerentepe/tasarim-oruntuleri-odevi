243bc58ec035b83623f6418599cd322044774d0e
ramazan_eren_tepe <teperamazaneren@gmail.com>
2026-05-12 11:41:37 +0300

    feat(faz-0): SifrelemeAraciApp sınıf iskeleti oluşturuldu

--- src/sifreleme/SifrelemeAraciApp.java
+++ src/sifreleme/SifrelemeAraciApp.java
@@ -1,9 +1,34 @@
 package sifreleme;
 
+import java.security.*;
+import java.util.Base64;
+import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 public class SifrelemeAraciApp {
 
-	public static void main(String[] args) {
+    private String algoritma;
+    private String anahtar;
 
-	}
+    public SifrelemeAraciApp(String algoritma, String anahtar) {
+        this.algoritma = algoritma;
+        this.anahtar = anahtar;
+    }
 
+    public String sifrele(String metin) {
+        // TODO
+    }
+
+    public String coz(String sifreliMetin) {
+        // TODO
+    }
+
+    private String padAnahtar(String anahtar) {
+        // TODO
+    }
+
+    public static void main(String[] args) {
+        // TODO
+    }
 }
