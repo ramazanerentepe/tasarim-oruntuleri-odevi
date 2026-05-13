37865dda4a6bac0ae866d9a691a05ea128f98be8
ramazan_eren_tepe <teperamazaneren@gmail.com>
2026-05-13 18:45:53 +0300

    refactor(faz-1): AesSifreleyici iskeletİ olusturuldu #3

---
+++ src/sifreleme/AesSifreleyici.java
@@ -0,0 +1,22 @@
+package sifreleme;
+
+public class AesSifreleyici implements Sifreleyici{
+    private String anahtar;
+    
+    public AesSifreleyici(String anahtar) {
+        this.anahtar = anahtar;
+    }
+
+	@Override
+	public String sifrele(String metin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String coz(String sifreliMetin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
