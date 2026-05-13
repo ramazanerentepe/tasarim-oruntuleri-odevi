9105c8cc3da79155b43846d8baf4be54af0d598a
ramazan_eren_tepe <teperamazaneren@gmail.com>
2026-05-13 19:44:09 +0300

    refactor(faz-1): Base64Sifreleyici iskeletİ olusturuldu #3

---
+++ src/sifreleme/Base64Sifreleyici.java
@@ -0,0 +1,17 @@
+package sifreleme;
+
+public class Base64Sifreleyici implements Sifreleyici{
+
+	@Override
+	public String sifrele(String metin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String coz(String sifreliMetin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
