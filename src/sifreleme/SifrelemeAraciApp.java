67d46d57749990a5c5f0e8c757823699c86b643e
ramazan_eren_tepe <teperamazaneren@gmail.com>
2026-05-14 20:45:18 +0300

    refactor(faz-1): main döngüsü ve UI iskeleti oluşturuldu #2

--- src/sifreleme/SifrelemeAraciApp.java
+++ src/sifreleme/SifrelemeAraciApp.java
@@ -7,12 +7,19 @@ public class SifrelemeAraciApp {
         Scanner scanner = new Scanner(System.in);
 		System.out.println("Şifreleme Aracı");
 		while (true) { 
-			System.out.println("Algoritma seçin (AES, RSA, BASE64) veya cikis için 'cikis':");
-			String algoritma = scanner.nextLine();
-			if(algoritma.equals("cikis")){
+			System.out.println("Algoritma seçin (AES, RSA, BASE64) veya cikis için 'CIKIS':");
+			String algoritma = scanner.nextLine().toUpperCase(); // Büyük küçük harf duyarlılığını önlemek için büyük harfe çeviriyoruz
+			if(algoritma.equals("CIKIS")){
 				System.out.println("Çıkış yapılıyor...");
 				break;
 			}
+			// TODO: factory ile seçilen algoritmaya göre şifreleme aracı oluşturuluyor
+			System.out.println("Islem seciniz (1: Sifrele / 2: Coz):");
+			String islem = scanner.nextLine();
+
+			System.out.println("Metin girin:");
+            String metin = scanner.nextLine();
+			// TODO : Seçilen algoritmaya göre şifreleme veya çözme işlemi yapılacak
 		}
 		scanner.close();
     }
