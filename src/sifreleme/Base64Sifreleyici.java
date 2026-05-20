b2efc78ea37b225d6b91dc5a200ad265a00888d1
ramazan_eren_tepe <teperamazaneren@gmail.com>
2026-05-20 11:33:03 +0300

    fix(faz-2): Base64Sifreleyici icindeki platform bagimli karakter kumesi hatası giderildi #5

--- src/sifreleme/Base64Sifreleyici.java
+++ src/sifreleme/Base64Sifreleyici.java
@@ -1,17 +1,18 @@
 package sifreleme;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class Base64Sifreleyici implements Sifreleyici{
 
 	@Override
 	public String sifrele(String metin) {
-		return Base64.getEncoder().encodeToString(metin.getBytes());
+		return Base64.getEncoder().encodeToString(metin.getBytes(StandardCharsets.UTF_8));
 	}
 
 	@Override
 	public String coz(String sifreliMetin) {
-		return new String(Base64.getDecoder().decode(sifreliMetin));
+		return new String(Base64.getDecoder().decode(sifreliMetin) ,StandardCharsets.UTF_8);
 	}
 
 }
