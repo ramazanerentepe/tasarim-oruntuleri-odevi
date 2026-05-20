1a664af1e7501e444f94865846c55d1083c9d809
ramazan_eren_tepe <teperamazaneren@gmail.com>
2026-05-20 11:29:36 +0300

    fix(faz-2): AesSifreleyici icindeki platform bagimli karakter kumesi hatası giderildi #5

--- src/sifreleme/AesSifreleyici.java
+++ src/sifreleme/AesSifreleyici.java
@@ -1,5 +1,6 @@
 package sifreleme;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -14,10 +15,10 @@ public class AesSifreleyici implements Sifreleyici{
 	@Override
 	public String sifrele(String metin) {
 		try {
-            SecretKeySpec secretKey = new SecretKeySpec(padAnahtar(anahtar).getBytes(), "AES");
+            SecretKeySpec secretKey = new SecretKeySpec(padAnahtar(anahtar).getBytes(StandardCharsets.UTF_8), "AES");
             Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            byte[] sifreliBytes = cipher.doFinal(metin.getBytes()); 
+            byte[] sifreliBytes = cipher.doFinal(metin.getBytes(StandardCharsets.UTF_8)); 
             return Base64.getEncoder().encodeToString(sifreliBytes);
 
         } catch (Exception e) {
@@ -28,12 +29,12 @@ public class AesSifreleyici implements Sifreleyici{
 	@Override
 	public String coz(String sifreliMetin) {
 		try {
-            SecretKeySpec secretKey = new SecretKeySpec(padAnahtar(anahtar).getBytes(), "AES");
+            SecretKeySpec secretKey = new SecretKeySpec(padAnahtar(anahtar).getBytes(StandardCharsets.UTF_8), "AES");
             Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
             byte[] sifreliBytes = Base64.getDecoder().decode(sifreliMetin);
             byte[] cozulmusBytes = cipher.doFinal(sifreliBytes);
-            return new String(cozulmusBytes);
+            return new String(cozulmusBytes , StandardCharsets.UTF_8);
 
         } catch (Exception e) {
             return "Hata: " + e.getMessage();
